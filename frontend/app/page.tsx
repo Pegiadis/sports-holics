cb1adc341e404ed25ca2f0e4a81b51adbe371d96
Pegiadis <ioannispegiadis97@gmail.com>
2025-12-12 22:37:49 +0200

    Update journalists

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -70,7 +70,7 @@ export default async function Home() {
     fetchCarouselNews(now),
     fetchMainNews(now),
     fetchLatestNews(now),
-    fetchJournalists(),
+    fetchJournalists(['aggelos-ntentas', 'giorgos-koyroy']),
     fetchHomepageNews(now),
     fetchHomepageFootball(now),
     fetchHomepageBasketball(now),
@@ -204,13 +204,6 @@ export default async function Home() {
                 <NewsCard key={index} {...news} />
               ))}
             </div>
-            {newsArticles.length > 3 && (
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8 mt-8">
-                {newsArticles.slice(3, 6).map((news, index) => (
-                  <NewsCard key={index} {...news} size="small" />
-                ))}
-              </div>
-            )}
           </section>
         )}
 
